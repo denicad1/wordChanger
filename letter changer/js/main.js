e800421cfe79338e748c7faba47ba4a2f9f1de36
Anthony Denicolo <denicoloa@live.bucks.edu>
2018-07-03 11:33:48 -0400

    still figuring out how to compare the vowels

--- letter changer/js/main.js
+++ letter changer/js/main.js
@@ -1,1 +1,18 @@
-﻿// Your code here!
+﻿function letterChanger(str) {
+//for loop to go through the whole string
+    for (var i = 0; i < str.length; i++) {
+        if (str[i]="a"||"e"||"i"||"o"||"u") {
+            str[i].toUpperCase();
+            console.log("it is a vowel")
+            console.log(str);
+        };
+    };
+};// Your code here!
+//letterChanger("ant");
+function alpha(c1 = 'a', c2 = 'z') {
+    //alphabet to compare it to and pick the letter after it
+    var a = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    return (a.indexOf(c1), a.indexOf(c2));
+};
+console.log(alpha());
+    
