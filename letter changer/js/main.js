0330365eba91333f5147b5b27f52d1404997d409
Anthony Denicolo <denicoloa@live.bucks.edu>
2018-07-06 14:20:45 -0400

    I am going to try to use a switch statement instead of if/else if statements to capitalize the vowels

--- letter changer/js/main.js
+++ letter changer/js/main.js
@@ -1,18 +1,32 @@
 ﻿function letterChanger(str) {
 //for loop to go through the whole string
+    str.split('');
+    var array = new Array("");
     for (var i = 0; i < str.length; i++) {
-        if (str[i]="a"||"e"||"i"||"o"||"u") {
-            str[i].toUpperCase();
-            console.log("it is a vowel")
-            console.log(str);
-        };
+        //try using a switch statement instead of using 5 if/else if statements
+        switch () {
+
+            default:
+        }
+        if (str[i] === "a" ) {
+            //trying to create a new array that has the letters from the original str and the capital letter vowels
+            array[i] = (str[i].toUpperCase());
+        } else {
+            array[i] = str[i];
+        }
+        
+        
+        console.log(array[i]);
+        
+        
+        
     };
 };// Your code here!
-//letterChanger("ant");
+letterChanger("ant");
 function alpha(c1 = 'a', c2 = 'z') {
     //alphabet to compare it to and pick the letter after it
     var a = 'abcdefghijklmnopqrstuvwxyz'.split('');
     return (a.indexOf(c1), a.indexOf(c2));
 };
-console.log(alpha());
+
     
