e84894eb4d48aef07ac69d1a4d30670ac9ac4148
Anthony Denicolo <denicoloa@live.bucks.edu>
2018-07-12 11:04:15 -0400

    letter changer now works

--- letter changer/js/main.js
+++ letter changer/js/main.js
@@ -1,9 +1,13 @@
 ﻿function letterChanger(str) {
-//for loop to go through the whole string
+//splitting the str into an array 
     str.split('');
+    //creating a new array to insert the new chars into
     var array = new Array("");
+    //alphabet to compare it to and pick the letter after it
+    var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    //for loop to go through the whole string
     for (var i = 0; i < str.length; i++) {
-        //forget switch case. try to compare consonants to the alphabet variable and get the next letter in the alphabet array
+        //if/else if statement that will cpaitalize all vowels
         if (str[i] === "a") {
             
             array[i] = str[i].toUpperCase();
@@ -16,26 +20,23 @@
         }//else if
             else if (str[i]==="o") {
             array[i] = str[i].toUpperCase();
-        }
+        }//else if
             else if (str[i]==="u") {
             array[i] = str[i].toUpperCase();
-        }
+        }//else if 
         else {
-            array[i] = str[i];
+            //another for loop to go through alphabet and compare the consonants 
+            for (var y = 0; y < a.length; y++) {
+                //if statement that will compare the chars of the string to the alphabet and pick the next letter in the alphabet and insert it into the new array
+                if (a[y] === str[i]) {
+                    array[i]=a[y + 1];
+                }//if
+            }//for
         }//else
-        
-        
-        console.log(array[i]);
-        
-        
-        
+        console.log(array[i]);  
     };//for loop
 };
 letterChanger("amphibian");
-function alpha(c1 = 'a', c2 = 'z') {
-    //alphabet to compare it to and pick the letter after it
-    var a = 'abcdefghijklmnopqrstuvwxyz'.split('');
-    return (a.indexOf(c1), a.indexOf(c2));
-};
+
 
     
